7838debe3fd34e9341dd0e3f090be811e9b62b8c
B-Halalipour <b.halalipour@gmail.com>
2026-02-10 07:35:20 +0000

    finished assignments for Values and Variables

--- 01-Fundamentals/assignments.js
+++ 01-Fundamentals/assignments.js
@@ -1,1 +1,7 @@
-const country 
+const country = "Iran"
+const continent = "Asia"
+const population = 90
+
+console.log("country:", country)
+console.log("continent:", continent)
+console.log("population:", population)
