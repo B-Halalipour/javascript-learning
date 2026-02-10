9c6fd194390d76b16b57a6def80e9c1f40911a2f
B-Halalipour <b.halalipour@gmail.com>
2026-02-10 08:56:29 +0000

    learned about data types

--- 01-Fundamentals/assignments.js
+++ 01-Fundamentals/assignments.js
@@ -1,3 +1,4 @@
+/*
 const country = "Iran"
 const continent = "Asia"
 const population = 90
@@ -5,3 +6,5 @@ const population = 90
 console.log("country:", country)
 console.log("continent:", continent)
 console.log("population:", population)
+
+*/
--- 01-Fundamentals/script.js
+++ 01-Fundamentals/script.js
@@ -1,10 +1,33 @@
-// let js = "amazing";
-// console.log(40 + 8 + 23 - 10);
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
 
-// console.log("Jonas")
-// console.log(23)
+console.log("Jonas")
+console.log(23)
 
-// let firstName = "Jonas"
-// console.log(firstName)
-// console.log(firstName)
-// console.log(firstName)
+let firstName = "Jonas"
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+*/
+
+// Data Types:
+/*
+let javaScriptIsFun = true;
+
+console.log(javaScriptIsFun)
+console.log(typeof javaScriptIsFun)
+
+
+javaScriptIsFun = "YES!"
+
+console.log(typeof javaScriptIsFun)
+
+let year;
+console.log(year)
+console.log(typeof year)
+
+year = 1991;
+console.log(typeof year)
+
+*/
