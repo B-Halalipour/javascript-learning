4610ea917f7ef7299a7bf56ce31b9d1ab1d2a45f
B-Halalipour <b.halalipour@gmail.com>
2026-02-11 06:06:42 +0000

    learned basic operators

--- 01-Fundamentals/script.js
+++ 01-Fundamentals/script.js
@@ -46,3 +46,35 @@ job = "teacher";
 console.log(job)
 
 */
+
+// Math Operators:
+const currentYear = 2037;
+const ageJonas = currentYear - 1991;
+const ageSarah = currentYear - 2020;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3;
+
+// we can use '+' operator to concatenate strings
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann"
+console.log(firstName + ' ' + lastName);
+
+// assignment operators:
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1;
+x--; // x = x - 1;
+x--
+console.log(x);
+
+
+//Comparison Operators:
+console.log(ageJonas > ageSarah); // >, <, >=, <=;
+console.log(ageSarah >= 18)
+
+const isFullAge = ageSarah >= 18;
+console.log(currentYear - 1991 > currentYear - 2018)
