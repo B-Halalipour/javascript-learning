1a364845af5284a6052d6f816e7182b8989660b4
B-Halalipour <b.halalipour@gmail.com>
2026-02-12 06:10:16 +0000

    learned if/else statement

--- 01-Fundamentals/script.js
+++ 01-Fundamentals/script.js
@@ -103,6 +103,7 @@ console.log(averageAge)
 */
 
 //Strings and Template literals:
+/*
 
 const firstName = "Jonas";
 const job = "teacher";
@@ -124,3 +125,29 @@ console.log("String \n\
 console.log(`String
     with
     multiple lines`)
+
+*/
+
+// if/else statements
+
+const age = 15;
+
+//const isOldEnough = age >= 18;
+
+if (age >= 18) {
+    console.log("Sarah can start driving license 🚗")
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. wait another ${yearsLeft} years :)`)
+}
+
+
+const birthYear = 1991;
+let century;
+if (birthYear <= 2000) {
+    century = 20
+} else {
+    century = 21
+}
+
+console.log(century)
