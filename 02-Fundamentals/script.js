ba1c3010d6f7e29d5be565af60ca853d42723e40
B-Halalipour <b.halalipour@gmail.com>
2026-03-23 06:03:31 +0000

    learned arrow function

--- 02-Fundamentals/script.js
+++ 02-Fundamentals/script.js
@@ -46,6 +46,8 @@ console.log(num)
 
 */
 
+/*
+
 // function declaration and expression
 // Parameter: is the place holder in the function
 // Argument: is the actual value that we use to fill in the place holder
@@ -68,3 +70,23 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1993)
 
 console.log(age1, age2)
+
+*/
+
+// Arrow Function:
+
+const calcAge3 = birthYear => 2026 - birthYear
+
+const age3 = calcAge3(1991)
+
+console.log(age3)
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2026 - birthYear
+    const retirement = 65 - age
+    // return retirement
+    return `${firstName} retires in ${retirement} years`
+}
+
+console.log(yearsUntilRetirement(1996, 'jonas'))
+console.log(yearsUntilRetirement(1993, 'behnoud'))
