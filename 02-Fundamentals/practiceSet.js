72eb742f844bd652dc5aaa83f4fc5094cd4ce681
B-Halalipour <b.halalipour@gmail.com>
2026-03-25 09:42:24 +0000

    created grade calculator function

--- 02-Fundamentals/practiceSet.js
+++ 02-Fundamentals/practiceSet.js
@@ -9,3 +9,19 @@ const isEven = function (num) {
 }
 
 // 3. Grade Calculator:
+
+const gradeCalc = function (score) {
+    if (score >= 90 && score <= 100) {
+        return "A"
+    } else if (score >= 80 && score <= 89) {
+        return "B"
+    } else if (score >= 70 && score <= 79) {
+        return "C"
+    } else if (score >= 60 && score <= 69) {
+        return "D"
+    } else if (score >= 0 && score < 60) {
+        return "F"
+    } else {
+        return "inputed number is not correct"
+    }
+}
