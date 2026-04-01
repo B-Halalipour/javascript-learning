12d37d6e905f0bb50d4950e188154271072ac81b
B-Halalipour <b.halalipour@gmail.com>
2026-04-01 18:18:30 +0100

    created add user function

---
+++ 02-Fundamentals/miniProject.js
@@ -0,0 +1,20 @@
+const users = ["Maya", "Devin", "Abbie"];
+
+//Task1: Adding a user if not existed:
+
+const addUser = function(usersArr, user) {
+    if(usersArr.includes(user)) {
+        console.log("user already exists");
+        
+    } else {
+        usersArr.push(user)
+        console.log("User added");
+    }
+
+    
+}
+console.log(users);
+
+addUser(users, 0)
+console.log(users);
+addUser(users,"Maya")
