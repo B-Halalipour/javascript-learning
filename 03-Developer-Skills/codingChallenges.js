02b310db0e9b3769d4b9494b1f74f05304f8ef88
B-Halalipour <b.halalipour@gmail.com>
2026-04-02 16:55:46 +0100

    solved challange 1 jonas solution

--- 03-Developer-Skills/codingChallenges.js
+++ 03-Developer-Skills/codingChallenges.js
@@ -14,17 +14,43 @@ TEST DATA 2: [12, 5, -5, 0, 4]
 */
 
 // My solution:
-const td1 = [17, 21, 23];
-const td2 = [12, 5, -5, 0, 4];
-const printForecast = function (tempArr) {
-  let finalMessage = '';
-  for (let i = 0; i < tempArr.length; i++) {
-    const message = `... ${tempArr[i]} C in ${tempArr.indexOf(tempArr[i]) + 1} days`;
-    finalMessage += message;
-  }
+// const td1 = [17, 21, 23];
+// const td2 = [12, 5, -5, 0, 4];
+// const printForecast = function (tempArr) {
+//   let finalMessage = '';
+//   for (let i = 0; i < tempArr.length; i++) {
+//     const message = `... ${tempArr[i]} C in ${tempArr.indexOf(tempArr[i]) + 1} days`;
+//     finalMessage += message;
+//   }
 
-  return finalMessage + ' ...';
+//   return finalMessage + ' ... ';
+// };
+
+// console.log(printForecast(td1));
+// console.log(printForecast(td2));
+
+// Jonas's solution:
+
+// 1) Understanding the problem
+// - Array transformed to string seperated by ...
+// - What is the X days? Answer: index + 1
+
+// 2) Breaking up into sub-problems
+// - Transforming array into string
+// - transform each element to string with C
+// - string needs to contain day
+// - add ... between elements and start and end of the string
+// - log string to console
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let str = '... ';
+  for (let i = 0; i < arr.length; i++) {
+    str = str + `${arr[i]}℃ in ${i + 1} days ... `;
+  }
+  console.log(str);
 };
 
-console.log(printForecast(td1));
-console.log(printForecast(td2));
+printForecast(data1);
