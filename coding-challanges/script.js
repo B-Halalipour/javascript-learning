d4b1faf8b8ba6da4ef7e6235268e365ce2bde172
B-Halalipour <b.halalipour@gmail.com>
2026-04-03 22:04:20 +0100

    solved challenge 2 using reusab le function as a methode in two objects

--- coding-challanges/script.js
+++ coding-challanges/script.js
@@ -1,3 +1,5 @@
+// Challenge 1:
+/*
 const behnoud = {
   firstName: 'Behnoud',
   lastName: 'Halalipour',
@@ -19,3 +21,36 @@ const behnoud = {
 const summary = behnoud.getSummary();
 console.log(summary);
 console.log(behnoud.age);
+*/
+
+//Challenge 2:
+calcAvg = function () {
+  let sum = 0;
+  for (let i = 0; i < this.scores.length; i++) {
+    sum += this.scores[i];
+  }
+  this.average = sum / this.scores.length;
+  return this.average;
+};
+const behnoud = {
+  fullName: 'Behnoud Halalipour',
+  scores: [20, 40, 30],
+  calcAvg,
+};
+const maryam = {
+  fullName: 'Maryam Haqparast',
+  scores: [20, 40, 60],
+  calcAvg,
+};
+const behnoudAvgScores = behnoud.calcAvg();
+const maryamAvgScores = maryam.calcAvg();
+
+if (behnoudAvgScores > maryamAvgScores) {
+  console.log(`Behnoud wins (${behnoudAvgScores} vs ${maryamAvgScores})`);
+} else if (maryamAvgScores > behnoudAvgScores) {
+  console.log(`Maryam wins (${maryamAvgScores} vs ${behnoudAvgScores})`);
+} else {
+  console.log(`Both have the same score ${behnoudAvgScores}`);
+}
+
+console.log(behnoud.average, maryam.average);
