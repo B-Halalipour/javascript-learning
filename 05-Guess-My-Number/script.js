3c0467434d392ec904347f2769036a7cb62f60bb
B-Halalipour <b.halalipour@gmail.com>
2026-04-06 13:58:14 +0100

    learned eventListener and eventHandler

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,5 +1,7 @@
 'use strict';
 // console.log(document.querySelector('.message'));
+
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number 🎉';
 console.log(document.querySelector('.message').textContent);
@@ -7,3 +9,11 @@ document.querySelector('.score').textContent = 12;
 document.querySelector('.highscore').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(guess, typeof guess);
+  if (!guess) document.querySelector('.message').textContent = 'No number ⛔️';
+});
