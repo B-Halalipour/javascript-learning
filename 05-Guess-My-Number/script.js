a421d7fd923b38db31e4c72215dcb335baa84c5f
B-Halalipour <b.halalipour@gmail.com>
2026-04-06 19:00:07 +0100

    implementing highscore feature

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -29,7 +29,8 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-    highscore = score;
+
+    if (score > highscore) highscore = score;
     document.querySelector('.highscore').textContent = highscore;
 
     // when guess is too high
