5b8bff9dc96ff1c15004462be2ad53636bdd92e1
B-Halalipour <b.halalipour@gmail.com>
2026-04-06 14:40:23 +0100

    game logic added: generate random number by refreshing, changing the message text to winner text if the number was right

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -10,10 +10,17 @@ document.querySelector('.highscore').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   console.log(guess, typeof guess);
-  if (!guess) document.querySelector('.message').textContent = 'No number ⛔️';
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number ⛔️';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number 🎉';
+  }
 });
