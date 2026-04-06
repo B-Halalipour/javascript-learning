2cb1bb65829e322157d6d18be89afa8ee8ba4191
B-Halalipour <b.halalipour@gmail.com>
2026-04-06 13:36:29 +0100

    reading value and manipulating the value of an input element

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -3,4 +3,7 @@
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number 🎉';
 console.log(document.querySelector('.message').textContent);
-document.querySelector('.number').textContent = '!';
+document.querySelector('.score').textContent = 12;
+document.querySelector('.highscore').textContent = 10;
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
