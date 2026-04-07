7807d0b774a2f78b35bafed6791b62a43945443b
B-Halalipour <b.halalipour@gmail.com>
2026-04-07 18:51:02 +0100

    learend keydown event and event object

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -3,7 +3,6 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
-console.log(btnsShowModal);
 
 const closeModal = function () {
   modal.classList.add('hidden');
@@ -21,3 +20,8 @@ for (let i = 0; i < btnsShowModal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log('a key was pressed');
+  console.log(e.key);
+});
