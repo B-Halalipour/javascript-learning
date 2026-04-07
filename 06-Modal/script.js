74673731c2f81f090666aeaf363fceab7450386b
B-Halalipour <b.halalipour@gmail.com>
2026-04-07 18:27:13 +0100

    create open modal function to add hidden class to overlay and modal window

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -4,22 +4,20 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 console.log(btnsShowModal);
+
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsShowModal.length; i++) {
-  btnsShowModal[i].addEventListener('click', function () {
-    console.log(`show modal ${i + 1} clicked`);
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsShowModal[i].addEventListener('click', openModal);
 }
 
-btnCloseModal.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
