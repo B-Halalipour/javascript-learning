3c60cc0fac4c347091a0b4573aa584625f2eef75
B-Halalipour <b.halalipour@gmail.com>
2026-04-07 18:04:51 +0100

    add event handler to show modal btns

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -6,5 +6,7 @@ const btnsShowModal = document.querySelectorAll('.show-modal');
 console.log(btnsShowModal);
 
 for (let i = 0; i < btnsShowModal.length; i++) {
-  console.log(btnsShowModal[i].textContent);
+  btnsShowModal[i].addEventListener('click', function () {
+    console.log(`show modal ${i + 1} clicked`);
+  });
 }
