6bc06a99bed6d22c4d7b0b15016a253cb2e1740c
B-Halalipour <b.halalipour@gmail.com>
2026-04-08 22:21:26 +0100

    pig-game complete

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -138,16 +138,28 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-
+let scores, currentScore, activePlayer, playing;
 // Starting conditions:
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--winner');
+};
+
+init();
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
@@ -203,3 +215,5 @@ btnHold.addEventListener('click', function () {
     // finish the game
   }
 });
+
+btnNew.addEventListener('click', init);
