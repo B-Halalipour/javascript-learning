9c1d40e8fce09cb95391ffb70c3f69cf360ea263
B-Halalipour <b.halalipour@gmail.com>
2026-04-13 09:36:12 +0100

    'var' is not block scoped but it's function scoped

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -2,7 +2,19 @@
 
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
-  console.log(firstName);
+  function printAge() {
+    const output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenail = true;
+      const str = `Oh, you are a millenial, ${firstName}`;
+      console.log(str);
+    }
+    // console.log(str);
+    console.log(millenail);
+  }
+  printAge();
   return age;
 }
 
