b206947fc6030a0efb74ce76ce8313de2e021f67
B-Halalipour <b.halalipour@gmail.com>
2026-04-15 07:45:43 +0100

    how to write clean code and avoid bugs declaring variables

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -36,3 +36,47 @@ calcAge(1991);
 */
 
 // Hoisting and TDZ
+/*
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+console.log(addExpr);
+console.log(addExpr(2, 3));
+console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+// any variable declared with var will be hoisted and set to undefined
+
+var addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+*/
+
+// Example
+
+/*
+console.log(numProducts);
+if (!numProducts) deleteShoppingCard();
+
+var numProducts = 10;
+
+function deleteShoppingCard() {
+  console.log('all products deleted');
+}
+*/
+
+// Use const most of the time to declare varables and let if you want to change the variable later
+// you should declare the varibales at top of your code
+// declare your functions first and use them only after decleration
