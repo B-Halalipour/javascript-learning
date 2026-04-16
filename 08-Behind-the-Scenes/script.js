1ef036bea65be7f216dfc359882eb9149b0bf074
B-Halalipour <b.halalipour@gmail.com>
2026-04-16 06:36:26 +0100

    this keyword in practice

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -80,7 +80,7 @@ function deleteShoppingCard() {
 // Use const most of the time to declare varables and let if you want to change the variable later
 // you should declare the varibales at top of your code
 // declare your functions first and use them only after decleration
-
+/*
 var x = 1;
 // declaring variable using var will create that property in global window object
 let y = 2;
@@ -88,3 +88,41 @@ const z = 3;
 console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
+*/
+
+// "This" key word
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  // in a regular function call the "this" keyword will point to undefined
+  console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => console.log(2037 - birthYear);
+// in an arrow functions it uses it's lexical "this" keyword
+console.log(this);
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    // when we have a methode call the "this" keyword inside of the methode will be the object that is calling the methode
+
+    console.log(2037 - this.year);
+  },
+};
+
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+// methode borrowing
+matilda.calcAge = jonas.calcAge;
+console.log(matilda);
+matilda.calcAge();
+
+const f = jonas.calcAge;
